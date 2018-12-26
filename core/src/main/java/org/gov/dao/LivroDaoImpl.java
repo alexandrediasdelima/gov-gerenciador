f8a4181e972e2ccda40bb74967e12970f9108d71
smj2081 <alexandre.lima@br.experian.com>
2018-12-26 18:22:52 -0200

    adicionando paginação e refatorando o livro dao

--- core/src/main/java/org/gov/dao/LivroDaoImpl.java
+++ core/src/main/java/org/gov/dao/LivroDaoImpl.java
@@ -15,16 +15,14 @@ public class LivroDaoImpl implements LivroDao {
 	 */
 	private static final long serialVersionUID = -5779326521469749102L;
 
+	EntityManager entityManager = new JPAUtil().getEntityManager();
 
 
 	public String gravar(Livro livro) {
 
-			EntityManager entityManager = new JPAUtil().getEntityManager();
-
 			entityManager.getTransaction().begin();
 			entityManager.persist(livro);
 			entityManager.getTransaction().commit();
-
 			entityManager.close();
 
 		return "ok";
@@ -32,40 +30,35 @@ public class LivroDaoImpl implements LivroDao {
 
 	public List<Livro> livros() {
 
-		EntityManager entityManager = new JPAUtil().getEntityManager();
-
 		entityManager.getTransaction().begin();
-
 		Query query = entityManager.createQuery("FROM Livro");
 
 		List<Livro> livros = query.getResultList();
 
 		entityManager.getTransaction().commit();
+		entityManager.close();
 
 		return livros;
 	}
 
 	public String remover(int id) {
 
-		EntityManager entityManager = new JPAUtil().getEntityManager();
-
 		entityManager.getTransaction().begin();
-
 		Livro livro = entityManager.find(Livro.class, id);
 
 		entityManager.remove(livro);
 		entityManager.getTransaction().commit();
+		entityManager.close();
 
 		return "ok";
 	}
 
 	public String editar(Livro livro) {
 
-		EntityManager entityManager = new JPAUtil().getEntityManager();
-
 		entityManager.getTransaction().begin();
 		entityManager.merge(livro);
 		entityManager.getTransaction().commit();
+		entityManager.close();
 
 		return "ok";
 
