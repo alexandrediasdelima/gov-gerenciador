63eaa52bbfed624daa34959a8aff1f70eab9fef8
smj2081 <alexandre.lima@br.experian.com>
2019-06-03 10:58:24 -0300

    confirm dialog

--- web/src/main/java/org/gov/web/controller/ApacBean.java
+++ web/src/main/java/org/gov/web/controller/ApacBean.java
@@ -43,6 +43,7 @@ public class ApacBean extends Controller  {
 
 		apacService.gravar(apac);
 		apacs();
+		setView(LIST);
 
 		return eval(index());
 	}
@@ -72,7 +73,7 @@ public class ApacBean extends Controller  {
 
 		apacService.editar(apac);
 		apacs();
-
+		setView(LIST);
 		return eval(index());
 	}
 
